38c3824927d8b5ba625c6704d1779ba55b8e4432
Koruro <koruro.web@gmail.com>
2022-07-14 11:41:51 +0200

    Added Ad before infographic if exists

--- components/Article/Data/ArticleData.tsx
+++ components/Article/Data/ArticleData.tsx
@@ -14,6 +14,7 @@ import {
   getArticleWordCount,
   getReadingTime,
 } from "../../../utils/articleMetrics";
+import PreInfographicAd from "../../Ads/PreInfographicAd";
 
 interface Props {
   article: Article;
@@ -43,6 +44,7 @@ const ArticleData: React.FC<Props> = ({
       />
       <ArticleContent slug={article.slug} content={article.content} />
       <ArticleReferedArticles articles={article.referedArticles} />
+      {article.infographic && <PreInfographicAd />}
       <ArticleInfographic infographic={article.infographic} />
       <ArticleVideo videoUrl={article.videoUrl} title={article.title} />
       <ArticleReferences references={article.references} />
