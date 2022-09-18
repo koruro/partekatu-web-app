f8529a776b9089f6b80455b18bf26667f0f82c82
Koruro <koruro.web@gmail.com>
2022-09-18 17:27:00 +0200

    Use inline styles for home page

--- components/Categories/CategoryList/CategoryList.tsx
+++ components/Categories/CategoryList/CategoryList.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import { useRouter } from "next/router";
+import { DetailedHTMLProps, HTMLAttributes } from "react";
 import { CategoriesEnum } from "../../../types/categories";
 import { CategoriesDict } from "../categories";
 import CategoryBox from "../CategoryBox/CategoryBox";
@@ -7,18 +8,22 @@ import OnlineCourse from "../OnlineCourse/OnlineCourse";
 import styles from "./styles.module.css";
 
 interface Props {
-  className?: string;
   categoryAs?: "h2" | "span";
   inColumn?: boolean;
 }
 
-const CategoryList: React.FC<Props> = ({ className, categoryAs, inColumn }) => {
+const CategoryList: React.FC<
+  Props & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+> = ({ categoryAs, inColumn, ...props }) => {
   const { asPath } = useRouter();
 
   const isCoursePage = asPath.includes("curso-euskera-online");
 
   return (
-    <div className={classNames(styles["container"], className)}>
+    <div
+      {...props}
+      className={classNames(styles["container"], props.className)}
+    >
       <div
         className={classNames(styles["category-list-box"], {
           [styles["category-list-box--wrap"]]: !inColumn,
