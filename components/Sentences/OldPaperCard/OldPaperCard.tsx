1ab92ee063cd2e2f2ae3a5ce1763b44207e225e6
Koruro <koruro.web@gmail.com>
2022-10-21 09:14:53 +0200

    Use simple old paper background. Remove svg filter for compatibility issues

--- components/Sentences/OldPaperCard/OldPaperCard.tsx
+++ components/Sentences/OldPaperCard/OldPaperCard.tsx
@@ -2,28 +2,7 @@ import { PropsWithChildren } from "react";
 import styles from "./styles.module.css";
 
 const OldPaperCard: React.FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <div>
-      <div className={styles["old-paper"]}>
-        <div className={styles["old-paper__parchment"]} id="parchment"></div>
-        <section className={styles["old-paper__contain"]} id="contain">
-          {children}
-        </section>
-      </div>
-      <svg style={{ display: "none" }}>
-        <filter id="wavy2">
-          <feTurbulence
-            x="0"
-            y="0"
-            baseFrequency="0.02"
-            numOctaves="5"
-            seed="1"
-          />
-          <feDisplacementMap in="SourceGraphic" scale="20" />
-        </filter>
-      </svg>
-    </div>
-  );
+  return <div className={styles["old-paper"]}>{children}</div>;
 };
 
 export default OldPaperCard;
