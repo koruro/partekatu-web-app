d401c65378b215238124a6b23f1e2e361a54877f
Koruro <79925033+koruro@users.noreply.github.com>
2023-05-01 17:40:16 +0200

    Rmoeve satic props. Change metadesc and title

--- pages/curso-euskera-online-2.tsx
+++ pages/curso-euskera-online-2.tsx
@@ -4,16 +4,14 @@ import CustomHead from "../components/CustomHead";
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
 import PageBox from "../components/Page/PageBox/PageBox";
-import EuskeraCourseContainer from "../containers/EuskeraCourseContainer/EuskeraCourseContainer";
 import { Article } from "../models/Article";
-import { articleRepository } from "../services/bootstrap";
 import { CategoriesEnum } from "../types/categories";
 import { genOnlineCourseStructuredData } from "../utils/structuredData";
 
-const headTitle = "El Curso de Euskera Online, Gratis y a tu manera";
-const metaTitle = "El Curso de Euskera Online, Gratis y a tu manera";
+const headTitle = "Curso de Euskera Online desde 0: Aprende Fácil y Rápido";
+const metaTitle = "Curso de Euskera Online desde 0: Aprende Fácil y Rápido";
 const metaDesc =
-  "Aquí aprenderás euskera desde 0 mediante lecciones cercanas que cualquiera entendería ¡Y, además, todo esto es gratis y a tu rimo!";
+  "¿Quieres hablar en euskera? Mediante este curso de euskera aprenderás de forma sencilla mediante vídeos, ejercicios e infografías ¡Entra y aprende!";
 const imgUrl =
   "https://images.ctfassets.net/vba2k9hz72yx/5TLW3PYNhNhNBQQtaevW1H/971296463ebe62e8fff85e9e82e2b16c/partekatu-online-course-image.png";
 
@@ -21,7 +19,7 @@ interface Props {
   articles: Record<CategoriesEnum, Article[]>;
 }
 
-const LegalPage: React.FC<Props> = ({ articles }) => {
+const LegalPage: React.FC<Props> = () => {
   return (
     <>
       <CustomHead
@@ -47,17 +45,7 @@ const LegalPage: React.FC<Props> = ({ articles }) => {
 
 // Get static props
 export const getStaticProps: GetStaticProps = async () => {
-  try {
-    // Fetch article and recommendations data
-    const landingArticles = await articleRepository.getLandingPageArticles();
-
-    return {
-      props: { articles: landingArticles },
-    };
-  } catch (error) {
-    console.error(error);
-    return { props: {} };
-  }
+  return { props: {} };
 };
 
 export default LegalPage;
