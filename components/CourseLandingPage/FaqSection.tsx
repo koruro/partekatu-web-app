e220e987629bd750d7948d965bb5ddf73be7a4a5
Koruro <79925033+koruro@users.noreply.github.com>
2023-05-27 09:30:19 +0200

    Update faq

--- components/CourseLandingPage/FaqSection.tsx
+++ components/CourseLandingPage/FaqSection.tsx
@@ -24,31 +24,41 @@ const FaqSection = () => {
             <li>
               <span>¿En cuánto tiempo podré finalizar el curso?</span>
               <p>
-                El curso tiene más de 2 horas de vídeos y ejercicios. Calculamos
-                que dedicándole unas 4 horas a la semana es posible terminarlo
-                en 2 o 3 semanas.
+                El curso tiene más de 2 horas de vídeos y, por otra parte,
+                ejercicios. Calculamos que dedicándole unas 4 horas a la semana
+                es posible terminarlo en 2 o 3 semanas.
               </p>
             </li>
             <li>
               <span>¿Conseguiré el nivel A1 de euskera con el curso?</span>
               <p>
-                Con este curso únicamente aprenderás euskera elemental y te
-                daremos las herramientas para comunicarte en situaciones
-                básicas. Todavía estarás lejos del nivel A1, que exige unas 125
-                horas de estudio de la lengua vasca.
-              </p>
-              <p>
-                Eso sí, obtendrás una sólida base para poder seguir tu camino
-                con el euskera.
+                Este curso te dará las claves principales para que entiendas el
+                euskera y te puedas comunicar en el menor tiempo posible. Es por
+                eso que no adquirirás el nivel A1 de la lengua vasca. Ahora,
+                obtendrás una sólida y útil base para poder seguir tu camino con
+                el euskera.
               </p>
             </li>
             <li>
               <span>¿Ofrecéis soporte para solucionar dudas?</span>
               <p>
-                El curso está pensado para ser breve, conciso y entendible. Es
-                por eso que en los propios vídeos nos anticipamos a las dudas
-                que puedas tener y te damos enlaces para profundizar en caso de
-                duda. Por eso no ofrecemos soporte por mail para aclarar dudas.
+                El curso está pensado para ser breve, conciso y entendible. En
+                los propios vídeos nos anticipamos a las dudas que puedas tener
+                y te damos enlaces para profundizar en caso de duda. Por eso no
+                ofrecemos soporte por mail para aclarar dudas.
+              </p>
+            </li>
+            <li>
+              <span>
+                ¿Y si no puedo permitirme el curso y quiero aprender euskera
+                gratis qué puedo hacer?
+              </span>
+              <p>
+                Creemos que poder estudiar euskera online debe estar al alcance
+                de cualquiera. Y por esa razón también puedes aprender euskera
+                de forma libre con los numerosos artículos educativos de nuestra
+                web. No obstante, en dichos artículos no tendrás una progresión
+                pensada para aprender euskera desde 0, ni vídeos, ni ejercicios.
               </p>
             </li>
           </ul>
