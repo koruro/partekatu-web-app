50eb60f8e299a5a78620116c7cc1c28473acb4ef
Koruro <79925033+koruro@users.noreply.github.com>
2023-10-28 11:07:21 +0200

    De nuevo lo mismo

--- components/CourseForm/CardCourseForm/BaseCourseForm.tsx
+++ components/CourseForm/CardCourseForm/BaseCourseForm.tsx
@@ -1,29 +1,5 @@
-import { Dispatch, SetStateAction } from "react";
-import SubscribeLoadingButton from "../LoadingButton/SubscribeLoadingButton";
 import styles from "./styles.module.css";
 
-interface Props {
-  email: string;
-  setEmail: Dispatch<SetStateAction<string>>;
-  setTermsAccepted: Dispatch<SetStateAction<boolean>>;
-  submitEnabled: boolean;
-  loading: boolean;
-
-  ignoreForm: () => any;
-  submitForm: () => any;
-}
-
-const BaseCourseForm: React.FC<Props> = ({
-  submitForm,
-  email,
-  setEmail,
-  ignoreForm,
-  loading,
-  setTermsAccepted,
-  submitEnabled,
-}) => {
-  return (
-    <>
       <div>
         <span
           style={{
